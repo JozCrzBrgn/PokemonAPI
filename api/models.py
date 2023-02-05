1994c5f61862efaf45f6f015c7360cf54dc0f6b3
Josue Cruz Barragan <47722525+JozCrzBrgn@users.noreply.github.com>
2023-02-04 19:57:10 -0600

    imag3

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class EntrenadorPokemon(models.Model):
+    region = models.CharField(max_length=50)
+    tipo = models.CharField(max_length=50)
+    numero_medallas = models.PositiveSmallIntegerField()
+
+
+
+
