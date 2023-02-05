74818c7ee28c94d7d72fddaed6c7b1dd391e80f9
Josue Cruz Barragan <47722525+JozCrzBrgn@users.noreply.github.com>
2023-02-04 23:04:21 -0600

    20 a 21

--- api/views.py
+++ api/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views import View
 from .models import EntrenadorPokemon
 from django.http.response import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 class EntrenadorPokemonView(View):
@@ -21,3 +24,18 @@ class EntrenadorPokemonView(View):
             else:
                 datos={'message':"Entrenador no encontrado..."}
             return JsonResponse(datos)
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request):
+        jd = json.loads(request.body)
+        print(jd)
+        EntrenadorPokemon.objects.create(
+            region = jd['region'], 
+            tipo = jd['tipo'], 
+            numero_medallas = jd['numero_medallas']
+            )
+        datos={'message':"Entrenador creado !"}
+        return JsonResponse(datos)
